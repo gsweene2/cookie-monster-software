7c388e41d5dfc0067ea36210f068f20ddc6c7994
gsweene2 <gsweene2@u.rochester.edu>
2018-07-28 02:45:19 -0700

    populate numbers on overview page

--- src/main/java/com/garrett/firstwebsite/person/PersonController.java
+++ src/main/java/com/garrett/firstwebsite/person/PersonController.java
@@ -4,6 +4,7 @@ import com.garrett.firstwebsite.Request.PrettyRequest;
 import com.garrett.firstwebsite.Request.Request;
 import com.garrett.firstwebsite.Request.RequestRepository;
 import com.garrett.firstwebsite.Request.RequestService;
+import com.garrett.firstwebsite.item.ItemService;
 import com.garrett.firstwebsite.profession.ProfessionService;
 
 import com.garrett.firstwebsite.user.User;
@@ -46,6 +47,9 @@ public class PersonController {
     @Autowired
     RequestService requestService;
 
+    @Autowired
+    ItemService itemService;
+
     /**
      * Get all persons
      */
@@ -84,93 +88,28 @@ public class PersonController {
 
     @GetMapping("/dashboard")
     public ModelAndView getUserDashboard(Authentication authentication, ModelAndView model){
-        model.setViewName("person/dashboard");
-        //get the current logged in person by
-        //using the Authentication to get EMAIL,
-        //and returning the user associated with the email
 
-        Validate.notNull(authentication.getName(), "Garrett: When loading dashbaord, authentication has no name");
-        User thisUser = userRepository.findByEmail(authentication.getName());
-        Validate.notNull(thisUser, "Garrett: When loading the dashboard, could not find the user in the user repo!");
-        //Log4J2LoggingSystem logger = new Log4J2LoggingSystem(ClassLoader.getSystemClassLoader());
+        // Count Items
+        int totalItems = itemService.getAllItem().size();
+        model.addObject("totalItems",totalItems);
 
+        // Count Users
+        int totalUsers = personService.getPersons().size();
+        model.addObject("totalUsers",totalUsers);
 
-
-        //Now that we have the user, we can get the userId
-        //and perform a search in the personRepository to find out
-        //if we have data for them
-
-        Validate.notNull(thisUser.getId(), "Garrett: The found User Object has a null userid");
-        Person thisPerson = personRepository.findByUserId(thisUser.getId());
-
-        // DO NOT VALIDATE IF THEY ARE NOT NULL, WILL BE CREATED IN NEXT BLOCK!
-        //Validate.notNull(thisUser.getId(), "Garrett: The current User obj's id is null");
-        //Validate.notNull(thisPerson, "Garrett: Could not find Person based on User id");
-
-        // ToDo: Make User class primary key a long!!
-
-        if (thisPerson == null){
-            model.addObject("personPassed", new Person());
-            model.addObject("professionsList", professionService.getProfessions());
-            model.setViewName("person/register");
-            return model;
-        }
-
-        long thispersonId = thisUser.getId();
-        personRepository.findByUserId(thispersonId);
-
-        //Now that we may have a person, we can determine if they need to fill out info!
-        model.addObject("thisuser",thisUser);
-        model.addObject("person", thisPerson);
-        model.addObject("profession",professionService.getProfession(Integer.toUnsignedLong(thisPerson.getProfessionId())).get());
-
-        // ************************************** //
-        // *********** Other Users ************** //
-        // ************************************** //
-
-        List<Person> otherUsers = personService.getPersons();
-        ArrayList<PrettyPerson> otherPrettyUsers = new ArrayList<PrettyPerson>();
-        for (Person p1 : otherUsers){
-            otherPrettyUsers.add(new PrettyPerson(p1.getId(),
-                    p1.getUserId(),
-                    //ToDo: find out why erroring out!
-                    userService.getPerson(p1.getUserId()).get().getName(),
-                    userService.getPerson(p1.getUserId()).get().getLastName(),
-                    professionService.getProfession(Integer.toUnsignedLong(p1.getProfessionId())).get().name
-                    ));
-        }
-        model.addObject("otherPrettyUsers", otherPrettyUsers);
-
-
-        // ************************************** //
-        // *********** User's Unfilled Request ************** //
-        // ************************************** //
-
+        // Count filled and unfilled requests
         List<Request> allRequests = requestService.getAllRequest();
-        ArrayList<PrettyRequest> prettyRequestsList = new ArrayList<>();
+        int totalFilledRequests = 0;
+        int totalUnfilledRequests = 0;
         for (Request req : allRequests){
-            if (req.getUserId() == thisUser.getId()){
-                if (req.getFilled() == 0){
-                    prettyRequestsList.add(requestService.requestToPrettyRequest(req));
+                if (req.getFilled() == 1){
+                    totalFilledRequests +=1;
+                } else {
+                    totalUnfilledRequests +=1;
                 }
-            }
         }
-        Collections.reverse(prettyRequestsList);
-
-        model.addObject("usersRequests",prettyRequestsList);
-
-
-        // ************************************** //
-        // *********** All Users Request ************** //
-        // ************************************** //
-
-        ArrayList<PrettyRequest> allPrettyRequests = new ArrayList<>();
-        for (Request req : allRequests){
-            allPrettyRequests.add(requestService.requestToPrettyRequest(req));
-        }
-        Collections.reverse(allPrettyRequests);
-
-        model.addObject("allRequests", allPrettyRequests);
+        model.addObject("totalFilledRequests",totalFilledRequests);
+        model.addObject("totalUnfilledRequests",totalUnfilledRequests);
 
         model.setViewName("person/dashboard");
         return model;
